187da971d7d6d4b547ee3bc5c0806e6186f31642
cssxsh <cssxsh@gmail.com>
2020-08-30 08:44:36 +0800

    style: default value for SimplePixivClient constructor

--- src/commonMain/kotlin/xyz/cssxsh/pixiv/client/SimplePixivClient.kt
+++ src/commonMain/kotlin/xyz/cssxsh/pixiv/client/SimplePixivClient.kt
@@ -1,5 +1,5 @@
 package xyz.cssxsh.pixiv.client
 
 expect open class SimplePixivClient(config: PixivConfig) : PixivClient, AbstractPixivClient {
-    constructor(block: PixivConfig.() -> Unit)
+    constructor(block: PixivConfig.() -> Unit = {})
 }
