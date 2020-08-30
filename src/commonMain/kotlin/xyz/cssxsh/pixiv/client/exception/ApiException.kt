48789b56daf1a4924a0fc763e48b1d57486eb8c4
cssxsh <cssxsh@gmail.com>
2020-08-30 10:00:49 +0800

    fix: ApiException message

--- src/commonMain/kotlin/xyz/cssxsh/pixiv/client/exception/ApiException.kt
+++ src/commonMain/kotlin/xyz/cssxsh/pixiv/client/exception/ApiException.kt
@@ -5,13 +5,13 @@ import io.ktor.client.statement.*
 import kotlinx.serialization.json.Json
 import xyz.cssxsh.pixiv.data.ApiError
 
-class ApiException(response: HttpResponse, val data: ApiError) : ResponseException(response) {
+class ApiException(response: HttpResponse, val apiError: ApiError) : ResponseException(response) {
     constructor(response: HttpResponse, json: String) : this(
         response,
         Json.decodeFromString(ApiError.serializer(), json)
     )
 
     override val message: String? = response.run {
-        "ApiException(url: ${call.request.url} invalid: ${status}, error: ${ApiError})"
+        "ApiException(url: ${call.request.url} invalid: ${status}, error: ${apiError})"
     }
 }
