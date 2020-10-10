ad4eefbe3fa22d39f09b4ccb5031738303a6909b
cssxsh <cssxsh@gmail.com>
2020-10-11 03:53:26 +0800

    fix: check

--- src/commonMain/kotlin/xyz/cssxsh/pixiv/client/exception/OtherClientException.kt
+++ src/commonMain/kotlin/xyz/cssxsh/pixiv/client/exception/OtherClientException.kt
@@ -2,9 +2,10 @@ package xyz.cssxsh.pixiv.client.exception
 
 import io.ktor.client.features.*
 import io.ktor.client.statement.*
+import io.ktor.util.*
 
 class OtherClientException(response: HttpResponse): ResponseException(response) {
     override val message: String = response.run {
-        "OtherException(url: ${call.request.url} invalid: ${status}, headers: ${headers}})"
+        "OtherException(url: ${call.request.url} invalid: ${status}, headers: ${headers.toMap()}})"
     }
 }
