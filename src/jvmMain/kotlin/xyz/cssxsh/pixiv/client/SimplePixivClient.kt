fc22b488df1b058793bf640ea397d59d213839d5
cssxsh <cssxsh@gmail.com>
2020-10-12 05:36:57 +0800

    debug: newBuilder

--- src/jvmMain/kotlin/xyz/cssxsh/pixiv/client/SimplePixivClient.kt
+++ src/jvmMain/kotlin/xyz/cssxsh/pixiv/client/SimplePixivClient.kt
@@ -126,16 +126,13 @@ actual constructor(
         engine {
             config {
                 addInterceptor { chain ->
-                    chain.request().let { request ->
-                        request.newBuilder().apply {
-                            // headers
-                            config.headers.forEach(::header)
-                            println(request.body.toString())
-                            authInfo?.let {
-                                header("Authorization", "Bearer ${it.accessToken}")
-                            }
-                        }.build()
-                    }.let {
+                    chain.request().newBuilder().apply {
+                        // headers
+                        config.headers.forEach(::header)
+                        authInfo?.let {
+                            header("Authorization", "Bearer ${it.accessToken}")
+                        }
+                    }.build().let {
                         chain.proceed(it)
                     }
                 }
