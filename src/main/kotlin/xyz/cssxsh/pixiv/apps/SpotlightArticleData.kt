a05d1163beb533e08098eac183f81a0f736f324a
cssxsh <cssxsh@gmail.com>
2021-04-28 07:08:55 +0800

    fix: SpotlightArticleData

--- src/main/kotlin/xyz/cssxsh/pixiv/apps/SpotlightArticleData.kt
+++ src/main/kotlin/xyz/cssxsh/pixiv/apps/SpotlightArticleData.kt
@@ -2,13 +2,11 @@ package xyz.cssxsh.pixiv.apps
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import xyz.cssxsh.pixiv.JapanDateTimeSerializer
-import java.time.OffsetDateTime
 
 @Serializable
 data class SpotlightArticleData(
     @SerialName("spotlight_articles")
     val articles: List<SpotlightArticle>,
     @SerialName("next_url")
-    val nextUrl: String,
+    val nextUrl: String?,
 )
