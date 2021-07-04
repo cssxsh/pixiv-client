5d61ad5e6ab2e9c20a69e881863b9ade1fd7c6de
cssxsh <cssxsh@gmail.com>
2021-07-04 16:03:07 +0800

    fix: build

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,10 +17,12 @@ repositories {
 
 dependencies {
     implementation(kotlinx("coroutines-core", Versions.coroutines))
+    implementation(ktor("client-auth", "1.6.1")) {
+        exclude(group = "io.ktor")
+    }
     implementation(ktor("client-core", Versions.ktor))
     implementation(ktor("client-serialization", Versions.ktor))
     implementation(ktor("client-encoding", Versions.ktor))
-    implementation(ktor("client-auth", "1.6.1"))
     implementation(ktor("client-okhttp", Versions.ktor))
     implementation(okhttp3("okhttp", Versions.okhttp))
     implementation(okhttp3("okhttp-dnsoverhttps", Versions.okhttp))
