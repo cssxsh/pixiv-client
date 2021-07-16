4a8c2f606d3de16578edff3ceac8dc6f55bd13b6
cssxsh <cssxsh@gmail.com>
2021-07-16 14:51:00 +0800

    style: ...

--- src/main/kotlin/xyz/cssxsh/pixiv/OtherAuth.kt
+++ src/main/kotlin/xyz/cssxsh/pixiv/OtherAuth.kt
@@ -1,9 +1,7 @@
 package xyz.cssxsh.pixiv
 
-import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.features.*
-import io.ktor.client.features.auth.*
 import io.ktor.client.request.*
 import io.ktor.client.request.forms.*
 import io.ktor.client.statement.*
