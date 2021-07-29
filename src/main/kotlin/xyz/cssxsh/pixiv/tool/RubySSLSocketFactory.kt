9f86869eb54813c589f976a410f9f12191649846
cssxsh <cssxsh@gmail.com>
2021-07-29 17:55:38 +0800

    fix: RubySSLSocketFactory

--- src/main/kotlin/xyz/cssxsh/pixiv/tool/RubySSLSocketFactory.kt
+++ src/main/kotlin/xyz/cssxsh/pixiv/tool/RubySSLSocketFactory.kt
@@ -10,33 +10,32 @@ object RubySSLSocketFactory : SSLSocketFactory() {
 
     private fun Socket.setServerNames(): Socket = apply {
         if (this !is SSLSocket) return@apply
-
         sslParameters = sslParameters.apply {
-            serverNames = serverNames.orEmpty().filter { name ->
+            serverNames = serverNames?.filter { name ->
                 if (name !is SNIHostName) return@filter false
                 regexes.none { it in name.asciiName }
             }
         }
     }
 
-    private val socketFactory: SSLSocketFactory = SSLContext.getDefault().socketFactory
+    private val default: SSLSocketFactory = SSLContext.getDefault().socketFactory
 
     override fun createSocket(socket: Socket?, host: String?, port: Int, autoClose: Boolean): Socket? =
-        socketFactory.createSocket(socket, host, port, autoClose)?.setServerNames()
+        default.createSocket(socket, host, port, autoClose)?.setServerNames()
 
     override fun createSocket(host: String?, port: Int): Socket? =
-        socketFactory.createSocket(host, port)?.setServerNames()
+        default.createSocket(host, port)?.setServerNames()
 
     override fun createSocket(host: String?, port: Int, localHost: InetAddress?, localPort: Int): Socket? =
-        socketFactory.createSocket(host, port, localHost, localPort)?.setServerNames()
+        default.createSocket(host, port, localHost, localPort)?.setServerNames()
 
     override fun createSocket(host: InetAddress?, port: Int): Socket? =
-        socketFactory.createSocket(host, port)?.setServerNames()
+        default.createSocket(host, port)?.setServerNames()
 
     override fun createSocket(address: InetAddress?, port: Int, localAddress: InetAddress?, localPort: Int): Socket? =
-        socketFactory.createSocket(address, port, localAddress, localPort)?.setServerNames()
+        default.createSocket(address, port, localAddress, localPort)?.setServerNames()
 
-    override fun getDefaultCipherSuites(): Array<String> = emptyArray()
+    override fun getDefaultCipherSuites(): Array<String> = default.defaultCipherSuites
 
-    override fun getSupportedCipherSuites(): Array<String> = emptyArray()
+    override fun getSupportedCipherSuites(): Array<String> = default.supportedCipherSuites
 }
