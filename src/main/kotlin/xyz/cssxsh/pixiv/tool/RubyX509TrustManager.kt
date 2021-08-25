9bec625a24ff9be18d03d8ca3b04c032add33488
cssxsh <cssxsh@gmail.com>
2021-08-25 20:20:21 +0800

    feat: RubyX509TrustManager.delegate

--- src/main/kotlin/xyz/cssxsh/pixiv/tool/RubyX509TrustManager.kt
+++ src/main/kotlin/xyz/cssxsh/pixiv/tool/RubyX509TrustManager.kt
@@ -1,13 +1,20 @@
 package xyz.cssxsh.pixiv.tool
 
-import java.security.cert.X509Certificate
-import javax.net.ssl.X509TrustManager
+import java.security.*
+import java.security.cert.*
+import javax.net.ssl.*
 
 object RubyX509TrustManager : X509TrustManager {
+
+    private val delegate: X509TrustManager by lazy {
+        val factory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+        factory.init(null as KeyStore?)
+        factory.trustManagers.filterIsInstance<X509TrustManager>().first()
+    }
 
     override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) = Unit
 
     override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) = Unit
 
-    override fun getAcceptedIssuers(): Array<X509Certificate> = emptyArray()
+    override fun getAcceptedIssuers(): Array<X509Certificate> = delegate.acceptedIssuers
 }
