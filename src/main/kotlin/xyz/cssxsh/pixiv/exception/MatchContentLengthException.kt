48fd9bfaab806516cd285c94584a7b942d862dc4
cssxsh <cssxsh@gmail.com>
2021-12-18 13:10:58 +0800

    fix: MatchContentLengthException

--- src/main/kotlin/xyz/cssxsh/pixiv/exception/MatchContentLengthException.kt
+++ src/main/kotlin/xyz/cssxsh/pixiv/exception/MatchContentLengthException.kt
@@ -1,9 +1,10 @@
 package xyz.cssxsh.pixiv.exception
 
+import io.ktor.client.features.*
 import io.ktor.client.statement.*
 import io.ktor.util.*
 
-class MatchContentLengthException(response: HttpResponse) : IllegalStateException() {
+class MatchContentLengthException(response: HttpResponse) : ResponseException(response, "<not match contentlength>") {
     init {
         response.content.cancel(this)
     }
