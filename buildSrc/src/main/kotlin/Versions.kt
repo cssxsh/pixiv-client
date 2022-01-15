ead0a031e2a1221b38b20e9ea6631b382ef54841
cssxsh <cssxsh@gmail.com>
2022-01-16 04:54:47 +0800

    fix: unused

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,4 +1,4 @@
-@file:Suppress("unused")
+
 
 object Versions {
     const val kotlin = "1.5.31"
@@ -6,7 +6,4 @@ object Versions {
     const val ktor = "1.5.4"
     const val okhttp = "4.9.2"
     const val gifencoder = "0.10.1"
-
-    const val shadow = "6.1.0"
-    const val junit = "5.6.2"
 }
