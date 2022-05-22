e9ebe3eff526590819af0827d61afb6e69a6def5
cssxsh <cssxsh@gmail.com>
2022-05-22 08:30:36 +0800

    update: dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    kotlin("jvm") version "1.6.0"
-    kotlin("plugin.serialization") version "1.6.0"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.serialization") version "1.6.21"
     id("net.mamoe.maven-central-publish") version "0.7.1"
 }
 
 group = "xyz.cssxsh.pixiv"
-version = "1.0.1"
+version = "1.0.2"
 
 mavenCentralPublish {
     useCentralS01()
@@ -19,31 +19,25 @@ repositories {
 }
 
 dependencies {
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-    api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
-    api("io.ktor:ktor-client-auth:1.6.5")
-    api("io.ktor:ktor-client-serialization:1.6.5") {
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+    api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+    api("io.ktor:ktor-client-auth:1.6.7")
+    api("io.ktor:ktor-client-serialization:1.6.7") {
         exclude("org.jetbrains.kotlinx", "kotlinx-serialization-json")
     }
-    api("io.ktor:ktor-client-encoding:1.6.5")
-    api("io.ktor:ktor-client-okhttp:1.6.5")
-    api("com.squareup.okhttp3:okhttp:4.9.2")
+    api("io.ktor:ktor-client-encoding:1.6.7")
+    api("io.ktor:ktor-client-okhttp:1.6.7")
     api("com.squareup.okhttp3:okhttp-dnsoverhttps:4.9.2")
     api("com.squareup:gifencoder:0.10.1")
     compileOnly("org.openpnp:opencv:4.5.1-2")
     compileOnly("org.seleniumhq.selenium:selenium-java:4.1.4")
 
-    testImplementation(kotlin("test", "1.6.0"))
+    testImplementation(kotlin("test", "1.6.21"))
     testImplementation("org.seleniumhq.selenium:selenium-java:4.1.4")
 }
 
 kotlin {
     explicitApi()
-    sourceSets {
-        all {
-            languageSettings.optIn("kotlin.RequiresOptIn")
-        }
-    }
     target.compilations {
         all {
             kotlinOptions {
